3d114910e7e1af50236785b67699dde86e7d9b16
EgorVladimirovich26 <egor-porohgmail.ru@yandex.ru>
2025-05-30 17:56:43 +0300

    Add files via upload

---
+++ DZ/data-types/data-types.js
@@ -0,0 +1,27 @@
+const myName = 'Егор';
+console.log('myName', myName, typeof myName);
+
+const myAge = 26;
+console.log('myAge', myAge, typeof myAge);
+
+const imHuman = true;
+console.log('imHuman', imHuman, typeof imHuman);
+
+const myAverageSleepValue = null;
+console.log('myAverageSleepValue', myAverageSleepValue, typeof myAverageSleepValue);
+
+let normalizingMySleep = undefined;
+console.log('normalizingMySleep', normalizingMySleep, typeof normalizingMySleep);
+
+const im = {
+    myName : 'Егор',
+    myAge : 26,
+    imHuman : true,
+};
+console.log('im', im, typeof im);
+
+const id = Symbol('id');
+console.log('id', id, typeof id);
+
+const someNumber = 100500n;
+console.log('someNumber', someNumber, typeof someNumber);
---
+++ DZ/data-types/dynamictyping.js
@@ -0,0 +1,11 @@
+let myAge = 26;
+myAge = '26';
+alert(myAge);
+
+let myName = 'Егор';
+myName = 100
+alert(myName);
+
+let imStudent = true;
+imStudent = null;
+alert(imStudent);
